5e3560a5e140a536e62a06046ea48d9d0cf862bb
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-05-07 21:36:09 +0900

    프로그래머스 리벨1단계

---
+++ src/Level1/N_number.java
@@ -0,0 +1,18 @@
+package Level1;
+
+class N_number {
+  public long[] solution(int x, int n) {
+    long[] answer = new long[n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        answer[i] += x;
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    N_number n = new N_number();
+    System.out.println(n.solution(2, 5));
+  }
+}
