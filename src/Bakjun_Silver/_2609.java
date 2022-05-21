17e5690fcfbfa20ec886ac7cf3a10bc6a5cfa7b0
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-05-21 23:27:17 +0900

    백준 실버 문제 최소공배수 최대공약수

---
+++ src/Bakjun_Silver/_2609.java
@@ -0,0 +1,42 @@
+package Bakjun_Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2609 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int num1 = Integer.parseInt(st.nextToken());
+    int num2 = Integer.parseInt(st.nextToken());
+    int max = 0;
+    int min = 0;
+
+    if (num1 > num2) {
+      max = num1;
+      min = num2;
+    } else {
+      max = num2;
+      min = num1;
+    }
+    int d = gcd(max, min);
+
+    System.out.println(d);
+    System.out.println(max * min / d);
+
+  }
+
+  public static int gcd(int a, int b) {
+
+    while (b != 0) {
+      int r = a % b;
+
+
+      a = b;
+      b = r;
+    }
+    return a;
+  }
+}
