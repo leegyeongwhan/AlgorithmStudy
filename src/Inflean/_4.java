c925fe5abd7550d3a53de8ff36ea6ddae2b8dda9
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-06-04 01:47:29 +0900

    백준 인프런 문자열 문제

--- src/Inflean/_4.java
+++ src/Inflean/_4.java
@@ -1,27 +1,37 @@
 package Inflean;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class _4 {
-  void solution(int num) {
-    Scanner scanner = new Scanner(System.in);
-    String str = "";
-    String reverse ;
-    for (int i = 0; i < num; i++) {
-      str = scanner.next();
-      reverse = "";
-      for (int j = str.length() - 1; j >= 0; j--) {
-        reverse += str.charAt(j);
-      }
-      System.out.println(reverse);
-      System.out.println();
+class Main {
+    ArrayList<String> solution(String[] arr) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (String s : arr) {
+            char[] chArr = s.toCharArray();
+            int len = chArr.length - 1;
+            for (int i = 0; i < chArr.length / 2; i++) {
+                char tmp = chArr[i];
+                chArr[i] = chArr[len];
+                chArr[len] = tmp;
+                len--;
+            }
+            list.add(String.valueOf(chArr));
+        }
+        return list;
     }
-   }
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int num = scanner.nextInt();
-    _4 t = new _4();
-    t.solution(num);
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        Main t = new Main();
+        String[] arr = new String[num];
+        for (int i = 0; i < num; i++) {
+            String str = scanner.next();
+            arr[i] = str;
+        }
+        for (String x : t.solution(arr)) {
+            System.out.println(x);
+        }
+
+    }
 }
