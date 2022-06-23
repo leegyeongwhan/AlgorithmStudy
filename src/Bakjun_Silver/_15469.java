4fd994f8a05442adfdd1bda48fe35ec7f1acb5d0
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-06-23 15:32:38 +0900

    백준 실버 15469 dfs 활용 문제 n 과 M

---
+++ src/Bakjun_Silver/_15469.java
@@ -0,0 +1,40 @@
+package Bakjun_Silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _15469 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        visit = new boolean[N];
+        arr = new int[M];
+
+        dfs(N, M, 0);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        /////////방문 한다음 꺼준다
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
