2382383381abe2577ed8c28bfd885551866e5fe2
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-07-10 19:03:48 +0900

    feat :백준 실버 2742 단순 반복문

---
+++ src/Bakjun/_2742.java
@@ -0,0 +1,16 @@
+package Bakjun;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class _2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for (int i = num; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
