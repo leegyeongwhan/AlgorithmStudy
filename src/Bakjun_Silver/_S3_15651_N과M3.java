8a8541d2595dbe7969b8fc08e8875206b4a33e7f
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-07-15 13:59:48 +0900

    feat :백준 실3 15651 n과m3 백트래킹 dfs

---
+++ src/Bakjun_Silver/_S3_15651_N과M3.java
@@ -0,0 +1,42 @@
+package Bakjun_Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _S3_15651_N과M3 {
+    static int N;
+    static int M;
+    static boolean[] visit;
+    static int[] arr;
+    static StringBuffer sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.valueOf(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N + 1];
+        arr = new int[M];
+        sb = new StringBuffer();
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+
+        }
+    }
+}
