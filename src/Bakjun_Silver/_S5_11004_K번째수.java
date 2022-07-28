dd24e9c37361c9ddd11a3dbac2dcf97801449eea
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-07-28 17:55:07 +0900

    feat : S5 11004 k번째 수 정렬

---
+++ src/Bakjun_Silver/_S5_11004_K번째수.java
@@ -0,0 +1,24 @@
+package Bakjun_Silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _S5_11004_K번째수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[M - 1]);
+    }
+}
