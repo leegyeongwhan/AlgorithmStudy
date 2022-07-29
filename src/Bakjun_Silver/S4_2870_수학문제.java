71e9184dda74e30de15b99fecf2835411dc9ccf5
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-07-29 15:46:34 +0900

    feat : S4 2870 정규표현식,정렬,split 활용하여 문제풀이

---
+++ src/Bakjun_Silver/S4_2870_수학문제.java
@@ -0,0 +1,17 @@
+package Bakjun_Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S4_2870_수학문제 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <n ; i++) {
+
+        }
+    }
+}
