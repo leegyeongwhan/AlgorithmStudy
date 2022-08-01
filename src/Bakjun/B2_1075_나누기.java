6f9b4adca0cd9c3ff0030dd0822102c873054ea1
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-08-02 00:04:21 +0900

    fix : b2 나누기 수정

--- src/Bakjun/B2_1075_나누기.java
+++ src/Bakjun/B2_1075_나누기.java
@@ -13,9 +13,12 @@ public class B2_1075_나누기 {
         n = (n / 100) * 100;
         while (n % m != 0) {
             n++;
-            n %= 100;
-            if(n < 10) System.out.println("0" + n);
-            else System.out.println(n);
+        }
+        n %= 100;
+        if (n < 10) {
+            System.out.println("0" + n);
+        } else {
+            System.out.println(n);
         }
     }
 }
