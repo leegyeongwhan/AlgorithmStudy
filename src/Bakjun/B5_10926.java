b55c067981447ad513bc503fb7e112fba43faf81
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-08-03 20:18:32 +0900

    feat : B5 10926

---
+++ src/Bakjun/B5_10926.java
@@ -0,0 +1,15 @@
+package Bakjun;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class B5_10926 {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        String lastString = "??!";
+
+        String s = in.next();
+        System.out.println(s + lastString);
+    }
+}
