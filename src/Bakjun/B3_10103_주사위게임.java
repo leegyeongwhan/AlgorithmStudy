e697c84bd52d68d58f15e8cb34276b753c81ac74
leegyeongwhan <leekhy02@naver.com>
2022-09-28 23:55:02 +0900

    feat : 간단 구현

---
+++ src/Bakjun/B3_10103_주사위게임.java
@@ -0,0 +1,29 @@
+package Bakjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B3_10103_주사위게임 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int ascore = 100;
+        int bscore = 100;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A > B) {
+                bscore -= A;
+            } else if (A < B) {
+                ascore -= B;
+            }
+        }
+        System.out.println(ascore + "\n" + bscore);
+    }
+}
