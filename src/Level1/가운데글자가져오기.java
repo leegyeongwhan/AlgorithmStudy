58d1807bf8201421b3f594eee0829354c6cd031f
leegyeongwhan <leekhy02@naver.com>
2022-10-10 20:05:36 +0900

    feat : 간단구현

---
+++ src/Level1/가운데글자가져오기.java
@@ -0,0 +1,19 @@
+package Level1;
+
+public class 가운데글자가져오기 {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        if (len % 2 == 0) { //짝수 홀수
+            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        } else {
+            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        가운데글자가져오기 s = new 가운데글자가져오기();
+        System.out.println(s.solution("abccd"));
+    }
+}
