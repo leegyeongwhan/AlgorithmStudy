0901fd4c5e549ffce3f19b3928b6eeebab38a8ce
leegyeongwhan <leekhy02@naver.com>
2022-10-21 23:57:05 +0900

    feat : 간단구현

---
+++ src/Bakjun/B3_9550_아이들은사탕을좋아해.java
@@ -0,0 +1,24 @@
+package Bakjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B3_9550_아이들은사탕을좋아해 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+        }
+    }
+}
