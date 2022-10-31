19145f9fd42784a749efc8b5e6cf3ce633360332
leegyeongwhan <leekhy02@naver.com>
2022-10-31 16:47:15 +0900

    feat : 코드스쿼드 별찍기 과제

---
+++ src/codesquad/B3_2522_별찍기12.java
@@ -0,0 +1,32 @@
+package codesquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B3_2522_별찍기12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
