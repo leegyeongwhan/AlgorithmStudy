858dd34435e08b990de66d6a1a48879bbde56541
leegyeongwhan <leekhy02@naver.com>
2022-10-31 21:34:42 +0900

    feat : 코드스쿼드 구구단 구현

---
+++ src/codesquad/구구단.java
@@ -0,0 +1,21 @@
+package codesquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 구구단 {
+    public static void main(String[] args) throws IOException {
+
+        for (int i = 2; i <= 9; i++) {
+            System.out.print(" " + i + "단 " + "      ");
+        }
+        System.out.println();
+        for (int k = 1; k <= 9; k++) {
+            for (int j = 2; j <= 9; j++) {
+                System.out.print(j + " x " + k + "     ");
+            }
+            System.out.println();
+        }
+    }
+}
