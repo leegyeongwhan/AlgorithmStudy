4afbd14a28f003cc5ca185309faa8404cd4e0337
leegyeongwhan <leekhy02@naver.com>
2022-11-01 16:45:05 +0900

    feat: 코드스쿼드 추가과제 5번

---
+++ src/codesquad/추과제5분기문익히기.java
@@ -0,0 +1,33 @@
+package codesquad;
+
+import java.util.Scanner;
+
+public class 추과제5분기문익히기 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("연산>>");
+        double a = scanner.nextDouble();
+        String oper = scanner.next();
+        double b = scanner.nextDouble();
+        System.out.print(a + oper + b + "의 계산 결과는 =");
+
+        switch (oper) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "/":
+                if (b == 0) {
+                    System.out.println("0으로 나눌수없습니다");
+                    return;
+                }
+                System.out.println(a / b);
+                break;
+        }
+    }
+}
