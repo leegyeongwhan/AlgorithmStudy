77ef77aad1b258fee3edf35c69556c84e82b406f
leegyeongwhan <leekhy02@naver.com>
2022-11-13 23:38:56 +0900

    feat(그리드): 탐욕법 문제

---
+++ src/Level1/체육복.java
@@ -0,0 +1,42 @@
+package Level1;
+
+import Bakjun.A;
+
+import java.util.Arrays;
+
+public class 체육복 {
+    // 1 3 5
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if ((lost[i] - 1 == reserve[j]) || (lost[i] + 1 == reserve[j])) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        체육복 s = new 체육복();
+        System.out.println(s.solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
+    }
+}
