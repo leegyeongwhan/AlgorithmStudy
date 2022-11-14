0d20846fcd082b2d27b7bdb346329a2dd6cceefd
leegyeongwhan <leekhy02@naver.com>
2022-11-14 22:54:52 +0900

    feat(문자열): 문자열 문제

---
+++ src/Lever2/JadenCase문자열만들기.java
@@ -0,0 +1,25 @@
+package Lever2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class JadenCase문자열만들기 {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += flag ? arr[i].toUpperCase() : arr[i];
+            flag = arr[i].equals(" ") ? true : false;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        JadenCase문자열만들기 s = new JadenCase문자열만들기();
+        System.out.println(s.solution("3people unFollowed    me   "));
+        System.out.println(s.solution("for the last week"));
+    }
+}
