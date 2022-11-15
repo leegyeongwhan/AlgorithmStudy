886058c056e59425f18ae6e8277502d753fb9b0e
leegyeongwhan <leekhy02@naver.com>
2022-11-15 16:26:53 +0900

    feat(수학): 수학문제

---
+++ src/Lever2/숫자의표현.java
@@ -0,0 +1,26 @@
+package Lever2;
+
+public class 숫자의표현 {
+    public int solution(int n) {
+        int answer = 1;
+        for (int i = 1; i <= n; i++) {
+            int sum = i;
+            for (int j = i + 1; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                if (sum > n) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        숫자의표현 s = new 숫자의표현();
+        System.out.println(s.solution(15));
+    }
+}
