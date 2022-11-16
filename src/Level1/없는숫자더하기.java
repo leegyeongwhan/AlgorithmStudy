06c57f39266000c10d0653d14c7a67906909c546
leegyeongwhan <leekhy02@naver.com>
2022-11-16 21:07:38 +0900

    feat(기본문제): 기본문제

---
+++ src/Level1/없는숫자더하기.java
@@ -0,0 +1,26 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class 없는숫자더하기 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        boolean[] arr = new boolean[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            arr[numbers[i]] = true;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i]) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        없는숫자더하기 m = new 없는숫자더하기();
+        m.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0});
+    }
+}
