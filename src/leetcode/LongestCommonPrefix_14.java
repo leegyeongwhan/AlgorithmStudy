2a63900248f2c774f305637ea8322ff7b9779581
leegyeongwhan <leekhy02@naver.com>
2022-11-23 20:47:11 +0900

    feat(리트코드): indexOf를 이용한 풀이

--- src/leetcode/LongestCommonPrefix_14.java
+++ src/leetcode/LongestCommonPrefix_14.java
@@ -1,13 +1,26 @@
-//package leetcode;
+package leetcode;
+
+public class LongestCommonPrefix_14 {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        if (strs.length == 0) {
+            return result;
+        }
+
+        result = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            String cur = strs[i];
+            while (cur.indexOf(result) != 0) {
+                result = result.substring(0, result.length()-1);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix_14 m = new LongestCommonPrefix_14();
+        System.out.println(m.longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+}
 //
-//public class LongestCommonPrefix_14 {
-//    public String longestCommonPrefix(String[] strs) {
-//        String result = "";
-//
-//        for (int i = 0; i < strs.length; i++) {
-//            for (int j = 0; j < strs[i].length(); j++) {
-//                strs[i].substring(i,j);
-//            }
-//        }
-//    }
-//}
