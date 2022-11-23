013f2b0ef1b0e09bef6ac984616aea0d4ca23fdf
leegyeongwhan <leekhy02@naver.com>
2022-11-23 16:38:18 +0900

    feat(리트코드): 로마 문자표현하기

---
+++ src/leetcode/LongestCommonPrefix_14.java
@@ -0,0 +1,13 @@
+//package leetcode;
+//
+//public class LongestCommonPrefix_14 {
+//    public String longestCommonPrefix(String[] strs) {
+//        String result = "";
+//
+//        for (int i = 0; i < strs.length; i++) {
+//            for (int j = 0; j < strs[i].length(); j++) {
+//                strs[i].substring(i,j);
+//            }
+//        }
+//    }
+//}
