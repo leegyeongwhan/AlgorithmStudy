7e5d2eff3861f42eada2d1f6db46fee53ca4256e
leegyeongwhan <leekhy02@naver.com>
2022-11-30 23:20:53 +0900

    feat(레벨1): 간단 구현

---
+++ src/programmers/Level1/음양더하기.java
@@ -0,0 +1,16 @@
+package programmers.Level1;
+
+public class 음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (!signs[i]) {
+                absolutes[i] *= -1;
+            }
+        }
+        for (int absolute : absolutes) {
+            answer += absolute;
+        }
+        return answer;
+    }
+}
