fdffe9a8a2d7bcee580b81e9e990f8eb70320342
leegyeongwhan <leekhy02@naver.com>
2022-12-13 23:47:01 +0900

    feat(레벨1): 간단 구현 수정

--- src/programmers/Level0/등수매기기.java
+++ src/programmers/Level0/등수매기기.java
@@ -7,14 +7,20 @@ public class 등수매기기 {
         int[] answer = new int[score.length];
 
         Arrays.fill(answer, 1);
-
+//[[1, 2], [1, 1], [1, 1]] -> [1, 2, 2]
         for (int i = 0; i < answer.length; i++) {
             for (int j = 0; j < answer.length; j++) {
-                if ((score[i][0] + score[i][1]) / 2 < (score[j][0] + score[j][1]) / 2) {
+                if ((score[i][0] + score[i][1]) < (score[j][0] + score[j][1])) {
                     answer[i]++;
                 }
             }
         }
+        System.out.println(Arrays.toString(answer));
         return answer;
+    }
+
+    public static void main(String[] args) {
+        등수매기기 m = new 등수매기기();
+        System.out.println(m.solution(new int[][]{{1, 2}, {1, 1}, {1, 1}}));
     }
 }
