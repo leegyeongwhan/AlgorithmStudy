cb9f5898973757b81f272e4f6a58bcffbb7b8935
leegyeongwhan <leekhy02@naver.com>
2022-12-23 21:59:49 +0900

    feat(레벨1): 간단구현

--- src/programmers/Level1/최소직사각형.java
+++ src/programmers/Level1/최소직사각형.java
@@ -2,15 +2,21 @@ package programmers.Level1;
 
 public class 최소직사각형 {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        int width = 0;
-        int height = 0;
+        int[] r = new int[sizes.length];
+        int[] l = new int[sizes.length];
+
         for (int i = 0; i < sizes.length; i++) {
-            int w = Math.max(sizes[i][0], sizes[i][1]);
-            int h = Math.min(sizes[i][0], sizes[i][1]);
-            width = Math.max(width, w);
-            height = Math.max(height, h);
+            r[i] = Math.max(sizes[i][0], sizes[i][1]);
+            l[i] = Math.min(sizes[i][0], sizes[i][1]);
         }
-        return answer = width * height;
+
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < r.length; i++) {
+            left = Math.max(left, r[i]);
+            right = Math.max(right, l[i]);
+        }
+        return left * right;
     }
 }
