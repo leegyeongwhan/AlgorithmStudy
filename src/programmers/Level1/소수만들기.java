d3f563132454eb6904d79febe67e4fd853361230
leegyeongwhan <leekhy02@naver.com>
2022-12-27 00:01:03 +0900

    feat(레벨1): 간단구현

--- src/programmers/Level1/소수만들기.java
+++ src/programmers/Level1/소수만들기.java
@@ -4,15 +4,19 @@ public class 소수만들기 {
     public int solution(int[] nums) {
         int answer = 0;
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = 1; j < nums.length - 1; j++) {
-                for (int k = 2; k < nums.length; k++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    boolean flag = true;
                     for (int z = 2; z < nums[i] + nums[j] + nums[k]; z++) {
                         if ((nums[i] + nums[j] + nums[k]) % z == 0) {
+                            flag = false;
                             break;
                         }
                     }
+                    if (flag) {
+                        answer++;
+                    }
                 }
-                answer++;
             }
         }
         return answer;
