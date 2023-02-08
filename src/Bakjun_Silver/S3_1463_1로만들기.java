5775bb7534c1e9aea2b58751365799288f4c709d
leegyeongwhan <leekhy02@naver.com>
2023-02-08 16:50:09 +0900

    feat(s3): dp

--- src/Bakjun_Silver/S3_1463_1로만들기.java
+++ src/Bakjun_Silver/S3_1463_1로만들기.java
@@ -8,18 +8,17 @@ public class S3_1463_1로만들기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
-        int dp[] = new int[number + 1];
+        int dp[] = new int[number + 1]; //10
 
         dp[0] = 0;
         dp[1] = 0;
 
-        for (int i = 2; i <= number; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        for (int i = 2; i < dp.length; i++) {
+            //  3을나눈 값 2를 나눈값 -1 을 한값중 최솟값을 구한다
+            dp[i] = dp[i - 1] + 1; //
+            if (i % 2 == 0) dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
         }
-
         System.out.println(dp[number]);
-        br.close();
     }
 }
